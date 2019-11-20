814fffa973b49d8f4e78ae3042b6488169395af3
sbeaudoinumaine <samuel.beaudoin@maine.edu>
2019-11-20 18:41:04 -0500

    Drop down menu, buttons, file input, and plot output example

--- app.R
+++ app.R
@@ -1,7 +1,41 @@
 library(shiny)
 
-ui <- fluidPage()
+ui <- fluidPage(
+  #Drop down for classifier selection
+  selectInput("classifierSelect", 
+              "Classifier:", 
+              c("Classifier 1", "Classifier 2", "Classifier 3")),
+  
+  #Button to generate a classifier
+  actionButton("generateClassifierButton", "Generate Classifier"),
+  
+  #File browser for data cubes
+  #accept gives the browser a hint of what kind of files the server is expecting.
+  fileInput("dataCubeFileInput", "Data Cube:", accept = c(".txt")),
+  
+  actionButton("plot", "New Plot"),
+  
+  plotOutput("hist")
+)
 
-server <-function(input, output) {}
+server <- function(input, output) {
+  observeEvent(input$generateClassifierButton, {
+    #this code is executed when the generateClassifierButton is pressed
+    print("button clicked")
+    
+  })
+  
+  #create an initial histogram with 100 random numbers
+  output$hist <- renderPlot({
+    hist(rnorm(100))
+  })
+  
+  observeEvent(input$plot, {
+    #create a histogram with 100 random numbers
+    output$hist <- renderPlot({
+      hist(rnorm(100))
+    })
+  })
+}
 
 shinyApp(ui = ui, server = server)
