e290e36bbc4903755f3b195082902047c5ddc018
sbeaudoinumaine <samuel.beaudoin@maine.edu>
2019-11-24 16:29:42 -0500

    Color changes + minor cleanup

--- app.R
+++ app.R
@@ -1,48 +1,41 @@
 library(shiny)
-
+library(shinyWidgets)
 
 ui <- fluidPage(
   tags$head(
     tags$style(HTML("
           .navbar .navbar-nav {float: right}
-          .navbar .navbar-header {float: left}
-          .navbar-inner { background-color: #121212 }
+          .navbar .navbar-header {float: left;}
+          .navbar-inner { background-color: #23262b }
           .navbar-static-top {
   position: static;
   margin-bottom: 0px;
-  background-color: #121212
-  
+  background-color: #23262b
+
           }
         "))
   ),
-  navbarPage("Spectral Site",
+  navbarPage("Hyperspectral Data Analyzer",
              tabPanel("Home",
                       br(),
                       br(),
                           sidebarLayout(
                             
-                            sidebarPanel(
+                            sidebarPanel(style = "background-color: #383a40; border-color: #383a40;",
                               #Change the background color of the Select Data/Generate Class tab buttons to black when selected and gray otherwise
                               tags$style(HTML("
-                                            .tabbable > .nav > li > a {background-color: #121212;  color:white}
-                                            .tabbable > .nav > li > a[data-value='Select Data'] {background-color: gray;   color:white}
-                                            .tabbable > .nav > li > a[data-value='Generate Class'] {background-color: gray;   color:white}
-                                            .tabbable > .nav > li[class=active] > a {background-color: black; color:white}
+                                            .tabbable > .nav > li > a {background-color: #383a40;  color:white}
+                                            .tabbable > .nav > li > a[data-value='Select Data'] {border-color: #2b2b2b; background-color: #2b2b2b;  color:white}
+                                            .tabbable > .nav > li > a[data-value='Generate Classifier'] {border-color: #2b2b2b; background-color: #2b2b2b;   color:white}
+                                            .tabbable > .nav > li[class=active] > a {border-color: #383a40; background-color: #383a40; color:white}
                               ")),
                               tabsetPanel(type = "tabs",
-                                          tabPanel("Select Data", 
-                                                   fluidRow(
-                                                     column(6,
-                                                            #Drop down for classifier selection
-                                                            selectInput("classifierSelect", 
-                                                                        "Classifier:", 
-                                                                        c("Classifier 1", "Classifier 2", "Classifier 3"))
-                                                     ),
-                                                     column(1,
-                                                            #Button to generate a classifier
-                                                            actionButton("selectClassifierButton", "Select")
-                                                     )
-                                                   ),
+                                          tabPanel("Select Data", style = "background-color: #383a40;",
+                                                   #Drop down for classifier selection
+                                                   selectInput("classifierSelect", 
+                                                               label = div(style="color: white;", "Classifier:"),
+                                                               c("Classifier 1", "Classifier 2", "Classifier 3")),
+                                                   
                                                    tags$head(tags$style(
                                                           HTML('
                                                             body, input, button, select { 
@@ -50,95 +43,61 @@ ui <- fluidPage(
                                                                 background-color: #121212;
                                                             }')
                                                    )),
-                                                   fluidRow(
-                                                     column(8,
-                                                            #File browser for data cubes
-                                                            #accept gives the browser a hint of what kind of files the server is expecting.
-                                                            fileInput("dataCubeFileInput", "Data Cube:", accept = c(".txt"))
-                                                     ),
+                                                   
+                                                   #File browser for data cubes
+                                                   #accept gives the browser a hint of what kind of files the server is expecting.
+                                                   fileInput("dataCubeFileInput", label = div(style="color: white;", "Data Cube:"),, accept = c(".txt")),
                                                      
-                                                     
-                                                   ),
-                                                   fluidRow(
-                                                     column(8,
-                                                            #File browser for data cubes
-                                                            #accept gives the browser a hint of what kind of files the server is expecting.
-                                                            textInput("Text Input", "Process Name:", value = "Enter text..."),
-                                                     ),
-                                                     
-
-                                                   ),
-                                                   fluidRow(
-                                        
-                                                     column(1,
-                                                            #Button to generate a classifier
-                                                            actionButton("addToQueueButton", "Add to Queue")
-                                                     )
-                                                     
-                                                   ),
-                                                   ),
-                                          tabPanel("Generate Class", 
-                                                   fluidRow(
-                                                     column(8,
-                                                            #File browser for data cubes
-                                                            #accept gives the browser a hint of what kind of files the server is expecting.
-                                                            textInput("Classifier", "Generate Classifier:", value = "Species"),
-                                                     ),
-                                                  
-                                                     column(1,
-                                                            #Button to generate a classifier
-                                                            actionButton("generateClassifierButton", "Generate")
-                                                     )
-                                                   ),
+                                                   
+                                                   #Button to generate a classifier
+                                                   actionButton("addToQueueButton", "Add to Queue")
+                                          ),
+                                          
+                                          tabPanel("Generate Classifier", style = "background-color:#383a40;",
+                                                   #File browser for data cubes
+                                                   #accept gives the browser a hint of what kind of files the server is expecting.
+                                                   textInput("speciesSelect", label = div(style="color: white;", "Species:"), value = "Species"),
+                                                   
                                                    tags$head(tags$style(
                                                      HTML('
                                                             body, input, button, select { 
                                                                 font-family: "Calibri";
-                                                                background-color: #121212;
+                                                                background-color: #23262b;
                                                             }')
                                                    )),
                                                   
-                                                   fluidRow(
-                                                     column(8,
-                                                            #File browser for data cubes
-                                                            #accept gives the browser a hint of what kind of files the server is expecting.
-                                                            textInput("ClassifierNameInput", "Classifier Name:", value = ""),
-                                                     ),
-                                                     
-                                                     
-                                                   ),
-                                                   fluidRow(
-                                                     
-                                                     column(1,
-                                                            #Button to generate a classifier
-                                                            actionButton("addClassifierButton", "Add Classifier")
-                                                     )
-                                                     
-                                                   ),
+                                                   #File browser for data cubes
+                                                   #accept gives the browser a hint of what kind of files the server is expecting.
+                                                   textInput("ClassifierNameInput", label = div(style="color: white;", "Classifier Name:"), value = ""),
                                                    
-                                                   )
+                                                   #Button to generate a classifier
+                                                   actionButton("generateClassifierButton", "Generate Classifier")
+                                                 
+                                        )
                                           
                               ),
                               
                             ),
                             
                             mainPanel(
+                              style = "background-color: #383a40;",
                               
-                              tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: red}")),
-                              tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: red}")),
-                              tags$style(HTML(".js-irs-2 .irs-single, .js-irs-2 .irs-bar-edge, .js-irs-2 .irs-bar {background: red}")),
-                              tags$style(HTML(".js-irs-3 .irs-single, .js-irs-3 .irs-bar-edge, .js-irs-3 .irs-bar {background: red}")),
-                             
-                              sliderInput("integer", "Integer:",
+                              #change the color of the min and max values on the slider to white
+                              tags$style(HTML(".irs-max {color: white;}
+                                              .irs-min {color: white;}")),
+                              
+                              setSliderColor(c("red", "red", "red", "red"), c(1, 2, 3, 4)),
+                              sliderInput("integer", label = div(style="color: white;", "Integer:"),
+                                          min = 0, max = 1000,
+                                          value = 500,
+                                          ),
+                              sliderInput("integer", label = div(style="color: white;", "Integer:"),
                                           min = 0, max = 1000,
                                           value = 500),
-                              sliderInput("integer", "Integer:",
+                              sliderInput("integer", label = div(style="color: white;", "Integer:"),
                                           min = 0, max = 1000,
                                           value = 500),
-                              sliderInput("integer", "Integer:",
-                                          min = 0, max = 1000,
-                                          value = 500),
-                              sliderInput("integer", "Integer:",
+                              sliderInput("integer", label = div(style="color: white;", "Integer:"),
                                           min = 0, max = 1000,
                                           value = 500),
                               
@@ -146,15 +105,14 @@ ui <- fluidPage(
                             )  
                         
                       ),
-                      
-                      
-                      
-                      
+          
                       img(src="logo.png", height="10%", width="10%", align="right")
              ),
+             
              tabPanel("Upload Data",
                       verbatimTextOutput("Upload Data")
              ),
+             
              tabPanel("View Data",
                       verbatimTextOutput("View Data")
              )
@@ -167,19 +125,6 @@ server <- function(input, output) {
   observeEvent(input$generateClassifierButton, {
     #this code is executed when the generateClassifierButton is pressed
     print("button clicked")
-    
-  })
-  
-  #create an initial histogram with 100 random numbers
-  output$hist <- renderPlot({
-    hist(rnorm(100))
-  })
-  
-  observeEvent(input$plot, {
-    #create a histogram with 100 random numbers
-    output$hist <- renderPlot({
-      hist(rnorm(100))
-    })
   })
 }
 
