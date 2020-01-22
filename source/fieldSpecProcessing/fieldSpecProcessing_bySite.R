299de2bd3eb19ae07ba8bce605f467fcb0dfeb34
Sam Beaudoin <samuel.beaudoin@maine.edu>
2020-01-22 12:55:20 -0500

    Process all Field Spec in a given directory

---
+++ source/fieldSpecProcessing/fieldSpecProcessing_bySite.R
@@ -0,0 +1,48 @@
+library(spectrolab)
+library(tidyverse)
+
+#fieldSpecDirectory is the directory to the folder containing the Field Spec data
+#the function will look at each directory under fieldSpecDirectory recursively and create a spectra.rds file
+
+processFieldSpec <- function(fieldSpecDirectory) {
+  directories <- list.dirs(path = fieldSpecDirectory, full.names = TRUE, recursive = TRUE)
+  
+  for (directory in directories) {
+    tryCatch({
+      
+      ####Read in data as spectra (all scans collected at this location)
+      spectra <- read_spectra(directory, format="sed")
+    
+      ##Fix Names 
+      names(spectra) <- gsub(".sed","",names(spectra))
+    
+      ###Create Metadata
+      metadata<-as.data.frame(names(spectra))
+      names(metadata)[1]<-"ScanID"
+    
+      ###Create column PFT and column area
+      metadata <- metadata %>% mutate(PFT=substr(metadata$ScanID,start = 1,stop = 6))
+    
+      separatedString <- strsplit(directory, "/")
+      area <- separatedString[[1]][4]
+      metadata$area <- area
+    
+      ##Set metadata
+      meta(spectra) = data.frame(metadata, stringsAsFactors = FALSE)
+    
+      fileName <- paste("output/fieldSpec", area, sep = "/")
+      fileName <- paste(fileName, "spectra.rds", sep = "_")
+    
+      ##save spectra (Raw)
+      saveRDS(spectra, fileName)
+    }, warning = function(warning) {
+      message <- paste("WARNING - While processing", directory)
+      message <- paste(message, warning, sep = " : ")
+      print(message)
+    }, error = function(error) {
+      message <- paste("ERROR - While processing", directory)
+      message <- paste(message, error, sep = " : ")
+      print(message)
+    })
+  }
+}
