e7a522d9872637d53c2672a61251e0034284997d
Matthew Prescott <47331985+Prescuit@users.noreply.github.com>
2020-01-23 18:21:55 -0500

    Created main.r file to auto run scripts

---
+++ source/main.R
@@ -0,0 +1,9 @@
+main <- function(){
+  source("source/fieldSpecProcessing/allPreds.r", echo = TRUE)
+  source("source/fieldSpecProcessing/bySite.r", echo = TRUE)
+  source("source/fieldSpecProcessing/createSpectralLibrary.r", echo = TRUE)
+  source("source/fieldSpecProcessing/resampled.r", echo = TRUE)
+  source("source/fieldSpecProcessing/VIs.r", echo = TRUE)
+}
+main()
+
