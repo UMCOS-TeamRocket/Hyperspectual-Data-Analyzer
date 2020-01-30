43a29f37f2000bfe6ea0ae811d12348546c7fc5b
Sam Beaudoin <samuel.beaudoin@maine.edu>
2020-01-30 12:11:58 -0500

    added progress bar for generating spectral library files

--- source/generateSpectralLibraryFiles.R
+++ source/generateSpectralLibraryFiles.R
@@ -5,27 +5,42 @@ source("source/fieldSpecProcessing/VIs.R")
 source("source/fieldSpecProcessing/allPreds.R")
 
 generateSpectralLibraryFiles <- function(spectraDirectories, name) {
-  tryCatch({
-    createSpectralLibrary(spectraDirectories, name)
-    
-    spectralLibraryRDSDirectory <- paste(paste("output/spectralLibraries/", name, sep = ""), ".rds", sep = "")
-    headwallSpectralLibrary(spectralLibraryRDSDirectory, name)
-    
-    hdwDfEqual25Directory <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_df_equal25.csv", sep = "")
-    fieldSpecResampled(hdwDfEqual25Directory, name)
-    
-    fieldSpecVI(hdwDfEqual25Directory, name)
-    
-    hdw10nm <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_010nm_equal25.csv", sep = "")
-    hdw50nm <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_050nm_equal25.csv", sep = "")
-    hdw100nm <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_100nm_equal25.csv", sep = "")
-    hdwVi <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_VIs_equal25.csv", sep = "")
-    hdwDirectories <- c(hdw10nm, hdw50nm, hdw100nm, hdwVi)
-    
-    fieldSpecAllPreds(hdwDirectories, name)
-  }, warning = function(warning) {
-    print(warning)
-  }, error = function(error) {
-    print(error)
+  withProgress(message = 'Generating Spectral Library Files', min = 0, max = 1, value = 0, {
+    tryCatch({
+      setProgress(0, detail = "Creating Spectral Library")
+      
+      createSpectralLibrary(spectraDirectories, name)
+      
+      setProgress(0.2, detail = "Creating Headwall Spectral Library")
+      
+      spectralLibraryRDSDirectory <- paste(paste("output/spectralLibraries/", name, sep = ""), ".rds", sep = "")
+      headwallSpectralLibrary(spectralLibraryRDSDirectory, name)
+      
+      setProgress(0.4, detail = "Resampling")
+      
+      hdwDfEqual25Directory <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_df_equal25.csv", sep = "")
+      fieldSpecResampled(hdwDfEqual25Directory, name)
+      
+      setProgress(0.6, detail = "VI")
+      
+      fieldSpecVI(hdwDfEqual25Directory, name)
+      
+      hdw10nm <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_010nm_equal25.csv", sep = "")
+      hdw50nm <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_050nm_equal25.csv", sep = "")
+      hdw100nm <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_100nm_equal25.csv", sep = "")
+      hdwVi <- paste(paste("output/hdwImagery/", name, sep = ""), "_HDW_VIs_equal25.csv", sep = "")
+      hdwDirectories <- c(hdw10nm, hdw50nm, hdw100nm, hdwVi)
+      
+      setProgress(0.8, detail = "All Preds")
+      
+      fieldSpecAllPreds(hdwDirectories, name)
+      
+      setProgress(1)
+    }, warning = function(warning) {
+      print(warning)
+    }, error = function(error) {
+      print(error)
+    })
   })
+ 
 }
