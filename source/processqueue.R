e0c7b1a95b5e106faec463abbf11cd43d0685584
Sam Beaudoin <samuel.beaudoin@maine.edu>
2020-01-30 11:54:43 -0500

    Progress bar for processing queue

--- source/processqueue.R
+++ source/processqueue.R
@@ -1,40 +1,53 @@
+library(shiny)
+library(shinyWidgets)
+
 source("source/imageModels/generateRFClassifier.R")
 source("source/imageModels/predict.R")
 source("source/imageProcessing/processHDWImage.R")
 
 processQueue <- function(queue) {
-  for (process in queue) {
-    tryCatch({
-      print("Current Process:")
-      print(process)
+  withProgress(message = 'Processing Queue', min = 0, max = length(queue), value = 0, {
+    index <- 0
+    for (process in queue) {
+      tryCatch({
+        classifier <- process[[1]]
+        spectralLibrary <- classifier[1]
+        mtry <- classifier[2]
+        ntree <- classifier[3]
+        importance <- classifier[4]
+        classifierFileName <- classifier[5]
+        
+        images <- process[[2]]
+        image <- images[1]
+        hdwImage <- images[2]
+        
+        outputFileName <- process[[3]]
+        
+        print(paste("Current Process:", outputFileName))
+        
+        print(index)
+        setProgress(index, detail = outputFileName)
+        
+        print("Generating RF Classifier")
+        classifierDirectory <- generateRFClassifier(classifierFileName, spectralLibrary, mtry, ntree, importance)
+        
+        print("Processing HDW Image")
+        hdwViDirectory <- processHDWImage(image)
+        
+        print("Predicting")
+        outputDirectory <- predict(classifierDirectory, image, hdwImage, hdwViDirectory, outputFileName)
+        
+        print("Process Finished")
+        
+        index <- index + 1
+      }, warning = function(warning) {
+        print(warning)
+      }, error = function(error) {
+        print(error)
+      })
       
-      classifier <- process[[1]]
-      spectralLibrary <- classifier[1]
-      mtry <- classifier[2]
-      ntree <- classifier[3]
-      importance <- classifier[4]
-      classifierFileName <- classifier[5]
-      
-      images <- process[[2]]
-      image <- images[1]
-      hdwImage <- images[2]
-      
-      outputFileName <- process[[3]]
-      
-      print("Generating RF Classifier")
-      classifierDirectory <- generateRFClassifier(classifierFileName, spectralLibrary, mtry, ntree, importance)
-      
-      print("Processing HDW Image")
-      hdwViDirectory <- processHDWImage(image)
-      
-      print("Predicting")
-      outputDirectory <- predict(classifierDirectory, image, hdwImage, hdwViDirectory, outputFileName)
-      
-      print("Process Finished")
-    }, warning = function(warning) {
-      print(warning)
-    }, error = function(error) {
-      print(error)
-    })
-  }
+      setProgress(length(queue))
+    }
+  })
+  
 }
