19d1e121248fe78d417e9e1d1715de6994c3b3ba
Sam Beaudoin <samuel.beaudoin@maine.edu>
2020-02-03 15:47:12 -0500

    Create output directory if not exist

---
+++ source/createOutputDirectories.R
@@ -0,0 +1,11 @@
+library(here)
+
+#create all output subdirectoies
+#TODO: showWarnings is set to FALSE in order to prevent a crash if the directories already exist. Another way around this?
+createOutputDirectories <- function() {
+  dir.create(file.path(here(), "output/classifiers"), recursive = TRUE, showWarnings = FALSE)
+  dir.create(file.path(here(), "output/fieldSpec"), recursive = TRUE, showWarnings = FALSE)
+  dir.create(file.path(here(), "output/hdwSpectralLibraries"), recursive = TRUE, showWarnings = FALSE)
+  dir.create(file.path(here(), "output/plots"), recursive = TRUE, showWarnings = FALSE)
+  dir.create(file.path(here(), "output/spectralLibraries"), recursive = TRUE, showWarnings = FALSE)
+}
