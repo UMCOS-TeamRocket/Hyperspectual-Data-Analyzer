e30b9ed2ee2645a66c4968d41d2a7c90c22e1558
Matthew Prescott <47331985+Prescuit@users.noreply.github.com>
2020-04-02 12:24:04 -0400

    Fixes to generating image
    -New function to generate images
    -Legend is now a seperate image

---
+++ source/imageModels/generateImage.R
@@ -0,0 +1,77 @@
+library(tidyverse)
+library(hsdar)
+
+generateImage <- function(raster, outputName, plotColors, plantVector) {
+    print(dim(raster))
+    print(ncol(raster))
+    print(nrow(raster))
+    
+    textSize<-10
+    textHoriz <- FALSE
+    
+    colIMG <- ncol(raster)
+    rowIMG <- nrow(raster)
+    
+    
+     if (colIMG > 2*rowIMG){
+       textHoriz <- TRUE
+       textSize <-5
+     }
+    
+    
+    if(colIMG > 1000){
+      sizeIMG <- 5
+    } else {
+      sizeIMG <- 50
+    }
+    
+    plotWidth <- colIMG*sizeIMG +3000
+    plotHeight <- rowIMG*sizeIMG
+  
+  
+  png(paste(paste("output/plots/", outputName, sep = ""), ".png", sep = ""), 
+      width=(plotWidth), 
+      height=(plotHeight)
+  )
+  plot(
+    raster,
+    legend = FALSE,
+    axes=FALSE,
+    col = plotColors,
+    box= FALSE
+  )
+
+   
+  dev.off()
+  
+  png(paste(paste("output/plots/LEGEND", outputName, sep = ""), ".png", sep = ""), 
+      width=(500), 
+      height=(1500)
+  )
+  plot(
+    raster,
+    legend = FALSE,
+    axes=FALSE,
+    col = "white",
+    box= FALSE
+  )
+  
+  legend(
+    "right",
+    xpd=TRUE,
+    legend = plantVector,
+    fill =plotColors,
+    border = FALSE,
+    bty = "n",
+    cex= 2.5,
+    xjust =1,
+    horiz = textHoriz,
+    inset = -0.007,
+    par(cex=0.4)
+    
+  )   
+  dev.off()
+  
+
+  return(paste(paste("output/plots/", outputName, sep = ""), ".png", sep = ""))
+  }
